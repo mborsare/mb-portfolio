7a0e6920b743a636ccee54ca3c808f7ad483ed9f
Mike Borsare <mborsare@gmail.com>
2014-08-18 13:45:52 -0400

    Added in fade between pages with js, updated email link, updated WAX comp

---
+++ js/fade-pages.js
@@ -0,0 +1,15 @@
+var speed = 'medium'; //slow, medium, fast
+
+$('html, body').hide();
+$(document).ready(function() {
+    $('html, body').fadeIn(speed, function() {
+        $('a[href], button[href]').click(function(event) {
+            var url = $(this).attr('href');
+            if (url.indexOf('#') == 0 || url.indexOf('javascript:') == 0) return;
+            event.preventDefault();
+            $('html, body').fadeOut(speed, function() {
+                window.location = url;
+            });
+        });
+    });
+});
